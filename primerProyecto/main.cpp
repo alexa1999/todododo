81839cb744e88348d7c921f8c173e80c53d8406c
alexa1999 <anel.veliz@ucsp.edu.pe>
2017-06-08 15:22:03 -0500

    Add files via upload

---
+++ primerProyecto/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+/*int main()
+{
+    float x;
+    cout<< "ingrese el valor xd x: ";
+    cin >> x;
+    float y ;
+    cout<< "ingrese el valor de y";
+    cin >> y;
+    float z = x +y;
+
+    cout << "La suma de " << x << " y " << y << " es igual a " << z <<endl;
+    z = x-y;
+    cout << "La resta de " << x << " y " << y << " es igual a " << z <<endl;
+    z = x*y;
+    cout << "La multiplicacion de " << x << " y " << y << " es igual a " << z <<endl;
+    z = x/y;
+    cout << "La division de " << x << " y " << y << " es igual a " << z <<endl;
+    return 0;
+}*/
+/*int main()
+{
+    float x;
+    cout<< "ingrese el valor de x: ";
+    cin >> x;
+    float y ;
+    cout<< "ingrese el valor de y:";
+    cin >> y;
+    float z;
+    cout<< "ingrese el valor de z:";
+    cin >> z;
+    float x/y*z
+    cout<<
+}*/
+int main()
+{
+    float r,p=3.14;
+    cout<< "ingrese el radio : ";
+    cin >> r;
+    cout <<" el area del circulo es :"<< p*r*r;
+
+
+}
