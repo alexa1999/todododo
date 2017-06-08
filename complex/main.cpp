ce0dc330696d5c4a56b6bb245eaac294e30c4f5a
alexa1999 <anel.veliz@ucsp.edu.pe>
2017-06-08 15:19:24 -0500

    Add files via upload

---
+++ complex/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+//#include "complejo.h"
+
+
+class Nimaginario{
+private:
+double real;
+double complejo;
+public:
+Nimaginario(double x,double y);
+suma(Nimaginario a,Nimaginario b);
+
+};
+
+Nimaginario::Nimaginario(double x, double y)
+{
+    real=x;
+    complejo=y;
+
+};
+Nimaginario::suma(Nimaginario a,Nimaginario b)
+{
+    Nimaginario a;
+    Nimaginario real;
+};
