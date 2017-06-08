22822736a2701531aac07ae2fedbc844161da12c
alexa1999 <anel.veliz@ucsp.edu.pe>
2017-06-08 15:22:13 -0500

    Add files via upload

---
+++ proyecto/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << " ANEL ESTOY MUY EMOCIONADA DE APRENDER C ++" << endl;
+    return 0;
+}
