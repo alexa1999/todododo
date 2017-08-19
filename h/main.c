2089f0521af9e5094c4eacfcf7860505b66fdc7c
alexa1999 <anel.veliz@ucsp.edu.pe>
2017-08-19 11:28:27 -0500

    Add files via upload

---
+++ h/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("dos mas dos es %d\n",4);
+    //cuando es f es flotante y e saldria un  0.0000
+    // con d ya me sale 4
+    // en el compilador se debe de usar gcc -Wall hola.c o hola
+    //este nos rive para mostarnos errores en el programna en el compilador
+    return 0;
+}
